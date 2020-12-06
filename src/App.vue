1ee18eec046cf3b26cd836eabe16f14dda5a677f
ATCamposs <andre.tatibano@gmail.com>
2020-12-06 09:45:21 -0300

    Como iniciar a construção com um array de objetos

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
     <!-- posso usar também <h1>{{ titulo }}</h1>-->
     <h1 v-text="titulo"></h1>
     <!-- no lugar de usar v-bind posso usar somente :atributo -->
-    <img :src="foto.url" :alt="foto.titulo" />
+    <ul>
+      <li><img :src="foto1.url" :alt="foto1.titulo" /></li>
+      <li><img :src="foto2.url" :alt="foto2.titulo" /></li>
+    </ul>
   </div>
 </template>
 
@@ -12,7 +15,24 @@ export default {
   data() {
     return {
       titulo: 'Banco de imagens de gatinhos',
-      foto: {
+      fotos: [
+        {
+          url:
+            'https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345_960_720.jpg',
+          titulo: 'Gato olhando para camera',
+        },
+        {
+          url:
+            'https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345_960_720.jpg',
+          titulo: 'Gato olhando para camera',
+        },
+      ],
+      foto1: {
+        url:
+          'https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345_960_720.jpg',
+        titulo: 'Gato olhando para camera',
+      },
+      foto2: {
         url:
           'https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345_960_720.jpg',
         titulo: 'Gato olhando para camera',
