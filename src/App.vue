7d6f6f852cc02d9ade8cf099ed45de0e3243aedd
ATCamposs <andre.tatibano@gmail.com>
2020-12-06 09:35:40 -0300

    Dentro de atributos não usar interpolação

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png" />
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <img src="{{ foto.url }}" alt="{{ foto.titulo }}" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
   data() {
     return {
-      msg: 'Bem vindo !',
+      titulo: 'Banco de imagens de gatinhos',
+      foto: {
+        url:
+          'https://cdn.pixabay.com/photo/2012/11/26/13/58/cat-67345_960_720.jpg',
+        titulo: 'Gato olhando para camera',
+      },
     }
   },
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+<style></style>
