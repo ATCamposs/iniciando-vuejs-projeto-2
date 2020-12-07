cfa067d9e4ded978e1fedaaf30629d7118490828
ATCamposs <andre.tatibano@gmail.com>
2020-12-07 15:08:40 -0300

    import e uso do router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
 Vue.use(VueResource)
-
+Vue.use(VueRouter)
 // eslint-disable-next-line no-new
 new Vue({
   el: '#app',
