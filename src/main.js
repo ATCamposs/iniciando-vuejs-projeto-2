55b554cff38b3f875d0f284399199880ffd04030
ATCamposs <andre.tatibano@gmail.com>
2020-12-08 16:15:18 -0300

    Criando e aplicando uma diretiva(pode ser aplicada em qualquer lugar)

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
+import './directives/Transform'
 
 Vue.use(VueResource)
 // Criação do routes e chamada da rota
