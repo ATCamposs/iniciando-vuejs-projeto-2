ca7a1ec31a7a2ee2d4718ef9224d38a7d4ba82d4
ATCamposs <andre.tatibano@gmail.com>
2020-12-08 17:19:27 -0300

    lazy só exibe o componente depois de um hover

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -11,8 +11,8 @@
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" v-model="foto.url" autocomplete="off" />
-        <imagem-responsiva />
+        <input id="url" v-model.lazy="foto.url" autocomplete="off" />
+        <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
       </div>
 
       <div class="controle">
