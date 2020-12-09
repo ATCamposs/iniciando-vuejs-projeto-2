20549554ed53f49b1ea1598ac0c20f8f0e8c2e5a
ATCamposs <andre.tatibano@gmail.com>
2020-12-08 22:09:02 -0300

    Como fazer um lazyLoad de um componente

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
-import Cadastro from './components/cadastro/Cadastro.vue'
+// import Cadastro from './components/cadastro/Cadastro.vue'
 import Home from './components/home/Home.vue'
+// ao passar para uma funcao, o vue irá chamar esse componente sob demanda criando um lazy load
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue')
 export const routes = [
   { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
   {
